90f9f581085299af208e142c3302e6bf4d3a5c06
Vitamax Oduol <vitamaxoduo@gmail.com>
2022-06-05 19:32:19 +0300

    My first toolbox!

--- 0x02-python-import_modules/1-calculation.py
+++ 0x02-python-import_modules/1-calculation.py
@@ -4,6 +4,6 @@ if __name__ == "__main__":
     a = 10
     b = 5
     print("{} + {} = {}".format(a, b, add(a, b)))
-    print("{} * {} = {}".format(a, b, add(a, b)))
+    print("{} x {} = {}".format(a, b, add(a, b)))
     print("{} - {} = {}".format(a, b, add(a, b)))
     print("{} / {} = {}".format(a, b, add(a, b)))
