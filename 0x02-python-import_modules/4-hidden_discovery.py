71b3088dbb4b9ea087548e8b2ccc501e96e5c289
Vitamax Oduol <vitamaxoduo@gmail.com>
2022-06-06 12:22:28 +0300

    correction

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -1,6 +1,13 @@
 #!/usr/bin/python3
-if __name__ == "__main__":
-    import hidden_4
-    for name in dir(hidden_4):
-        if name[0] != '_' and name[1] != '_':
+
+
+def print_hidden_names():
+    import hidden_4 as hidden
+
+    for name in dir(hidden):
+        if not name.startswith('__'):
             print(name)
+
+
+if __name__ == "__main__":
+    print_hidden_names()
