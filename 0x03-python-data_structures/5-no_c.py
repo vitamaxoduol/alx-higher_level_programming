6e3c51eeccfeb3f1ef692a49f6fb20461c90d914
Vitamax Oduol <vitamaxoduo@gmail.com>
2022-06-06 11:11:30 +0300

    removes all characters c and C from a string

---
+++ 0x03-python-data_structures/5-no_c.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+
+
+def no_c(my_string):
+    for x in my_string:
+        if x in "Cc":
+            my_string = my_string.split(x)
+            my_string = "".join(my_string)
+    return my_string
