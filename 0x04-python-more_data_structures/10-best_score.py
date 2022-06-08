1150858dbf960b47cc46fe6e52e996359bb44c87
Vitamax Oduol <vitamaxoduo@gmail.com>
2022-06-09 02:07:21 +0300

    bigger score

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -4,7 +4,7 @@
 def best_score(a_dictionary):
     if a_dictionary is None:
         return None
-    keys, value = list(a_dictionary.keys()), list(a_dictionary.values())
+    keys, values = list(a_dictionary.keys()), list(a_dictionary.values())
     score = sorted(list(zip(keys, values)), key=lambda x: X[1], reverse=True)
 
     return score[0][0]
