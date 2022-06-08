805d2001cfa3eb083710e39e8df4261829425465
Vitamax Oduol <vitamaxoduo@gmail.com>
2022-06-09 01:23:29 +0300

    Simple delete by key

--- 0x04-python-more_data_structures/8-simple_delete.py
+++ 0x04-python-more_data_structures/8-simple_delete.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 
+def simple_delete(a_dictionary, key=""):
+    if a_dictionary.get(key) is not None:
+        del a_dictionary[key]
 
-def update_dictionary(a_dictionary, key, value):
-    a_dictionary[key] = value
     return a_dictionary
