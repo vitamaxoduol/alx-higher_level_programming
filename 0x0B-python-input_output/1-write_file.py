c41c29a6d519c4ab09588aacbdb13778eb537939
Vitamax Oduol <vitamaxoduo@gmail.com>
2022-07-06 04:41:46 +0300

    correcting the file

--- 0x0B-python-input_output/1-write_file.py
+++ 0x0B-python-input_output/1-write_file.py
@@ -6,5 +6,5 @@ returns the number of characters"""
 def write_file(filename="", text=""):
     """ returns the number of characters written"""
 
-    with open(filename="", "r", encoding="utf-8") as file:
+    with open(filename="", "w", encoding="utf-8") as file:
         return (file.write(text))
