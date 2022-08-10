42cba96b32759ea4e94834f9f010d00c0a2646fc
Vitamax Oduol <vitamaxoduo@gmail.com>
2022-08-11 02:14:30 +0300

    My previledges & root users

--- 0x0E-SQL_more_querie/1-create_user.sql
+++ 0x0E-SQL_more_querie/1-create_user.sql
@@ -3,7 +3,6 @@
 -- The user_0d_1 password should be set to user_0d_1_pwd
 -- If the user user_0d_1 already exists, your script should not fail
 
-CREATE USER 
-IF NOT EXISTS
-'user_0d_1'@'localhost' IDENTIFIED WITH mysql_native_password BY 'user_0d_1_pwd';
-GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost'
+CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
+GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost';
+FLUSH PRIVILEDGES;
