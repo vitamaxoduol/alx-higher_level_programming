cf94ad64899b04fed498db3feb0baee6ec683578
Vitamax Oduol <vitamaxoduo@gmail.com>
2022-09-06 21:09:51 +0300

    commit changes

--- 0x12-javascript-warm_up/1-multi_languages.js
+++ 0x12-javascript-warm_up/1-multi_languages.js
@@ -1,6 +1,6 @@
 #!/usr/bin/node
-
-const firstLine = 'C is fun';
-const secondLine = 'Python is cool';
-const thirdLine = 'JavaScript is amazing';
-console.log(firstLine, secondLine, thirdLine);
+// const firstLine = 'C is fun';
+// const secondLine = 'Python is cool';
+// const thirdLine = 'JavaScript is amazing';
+// console.log(firstLine, secondLine, thirdLine);
+console.log('C is fun\nPython is cool\nJavaScript is amazing');
