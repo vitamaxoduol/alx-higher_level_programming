a64c7c0ed0c136f4f40e0f95b963a495e9240a51
Vitamax Oduol <vitamaxoduo@gmail.com>
2022-09-12 14:33:52 +0300

    update

--- 0x12-javascript-warm_up/2-arguments.js
+++
@@ -1,3 +0,0 @@
-#!/usr/bin/node
-const args = process.argv.length;
-console.log(args === 2 ? 'No argument' : args === 3 ? 'Argument found' : 'Arguments found');
--- 0x12-javascript-warm_up/6-multi_languages_loop.js
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/node
-
-const languages = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
-
-languages.forEach((language) => {
-  console.log(language);
-});
