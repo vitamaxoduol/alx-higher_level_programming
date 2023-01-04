00bd599b4d3d2339dfe53c88436059c5233ed08d
vitamaxoduol <vitamaxoduol@gmail.com>
2023-01-04 15:17:35 +0300

    listing all previledges

--- 0x0E-SQL_more_querie/0-privileges.sql
+++ 0x0E-SQL_more_querie/0-privileges.sql
@@ -1,3 +1,5 @@
--- lists all privileges of the MySQL users user_0d_1 
+-- a script that lists all privileges of
+-- the MySQL users user_0d_1 and user_0d_2 on your serve
+
 SHOW GRANTS FOR 'user_0d_1'@'localhost';
 SHOW GRANTS FOR 'user_0d_2'@'localhost';
